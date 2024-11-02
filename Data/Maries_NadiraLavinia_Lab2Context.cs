c4951019f1747d2c364f0d7d9f89f812a4585eeb
Nadira Maries <maries.lavinia@gmail.com>
2024-11-02 15:14:30 +0200

    Add project files.

---
+++ Data/Maries_NadiraLavinia_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Maries_NadiraLavinia_Lab2.Models;
+
+namespace Maries_NadiraLavinia_Lab2.Data
+{
+    public class Maries_NadiraLavinia_Lab2Context : DbContext
+    {
+        public Maries_NadiraLavinia_Lab2Context (DbContextOptions<Maries_NadiraLavinia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Maries_NadiraLavinia_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Maries_NadiraLavinia_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
